92a64e415b750868d8fe22fa7b791627d200daeb
Marcelo Arthur <marcelobragalemos@gmail.com>
2019-05-07 15:52:11 -0300

    _

--- machine.js
+++ machine.js
@@ -8,12 +8,40 @@ document.getElementById("sete").addEventListener("click", gameTicTac)
 document.getElementById("oito").addEventListener("click", gameTicTac)
 document.getElementById("nove").addEventListener("click", gameTicTac)
 
+const player = ["x", "o"]
+const pl1 = [] // pl1 = O
+const pl2 = [] // pl2 = X
 
-let player = ["x", "o"]
+// function verifWin() {
+//     const possiblewins = [
+//         ["um", "dois", "tres"],
+//         ["um", "quatro", "sete"],
+//         ["um", "cinco", "nove"],
+//         ["dois", "cinco", "oito"],
+//         ["quatro", "cinco", "seis"],
+//         ["sete", "oito", "nove"],
+//         ["sete", "cinco", "tres"],
+//     ]
+
+//     possiblewins.forEach(element => {
+//         if (pl1.find(element) || pl2.find(element)) {
+//             alert("Vencedor!")
+//         } else {
+//             alert("IH, DEU VELHA")
+//         }
+//     });
+// }
 
 function gameTicTac() {
-    let playAlternate = player.reverse()
-    this.className += " "+playAlternate[0]
-    player = playAlternate
+    // verifWin()
+    const playAlternate = player.reverse()
+    this.className += " " + playAlternate[0]
+    if (playAlternate[0] === "x") {
+        pl2.push(this.id)
+    } else {
+        pl1.push(this.id)
+    }
 
+    console.log(pl1)
+    console.log(pl2)
 }
