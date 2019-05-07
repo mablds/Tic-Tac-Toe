d8cd0e634a7725983ec637510fc45c151b7bed2f
Marcelo Arthur <marcelobragalemos@gmail.com>
2019-05-07 17:27:08 -0300

    _

--- machine.js
+++ machine.js
@@ -11,6 +11,7 @@ document.getElementById("nove").addEventListener("click", gameTicTac)
 const player = ["x", "o"]
 const pl1 = [] // pl1 = O
 const pl2 = [] // pl2 = X
+const finalArray = []
 const possiblewins = [
     ["um", "dois", "tres"],
     ["um", "quatro", "sete"],
@@ -21,15 +22,21 @@ const possiblewins = [
     ["sete", "cinco", "tres"],
 ]
 
-function verifWin() {
-    possiblewins.forEach(element => {
-        if (pl1.equals(element) || pl2.equals(element)) {
-            alert("Jogo encerrado! Temos um vencedor.")
-        }
-        if (pl1[5] || pl2[5]) {
-            alert("Ops! Encontramos um erro")
+function verifWin(winArr, playerArr) {
+    //se o "player"(playerArr) tem a condição de vitoria (todos os elementos dentro do winArr) ... 
+    // winArr = ["um", "dois", "tres"]
+    // playerArr = ["um", "dois", "hehehe", "huhuhu", "milão", "trjes"]
+    let verif = false
+    winArr.map(e => {
+        if (playerArr.includes(e)) {
+            console.log('iguais')
+            verif = true
         }
     })
+    // console.log('iguais')
+    if(verif == true){
+        alert("Temos um vencedor!")
+    }
 }
 
 function gameTicTac() {
@@ -37,12 +44,9 @@ function gameTicTac() {
     this.className += " " + playAlternate[0]
     if (playAlternate[0] === "x") {
         pl2.push(this.id)
-        verifWin()
+        verifWin(possiblewins, pl2)
     } else {
         pl1.push(this.id)
-        verifWin()
+        verifWin(possiblewins, pl1)
     }
-
-    console.log(pl1)
-    console.log(pl2)
 }
