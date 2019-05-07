ed9229b32d13688075e996b41693502dcfef3818
Marcelo Arthur <marcelobragalemos@gmail.com>
2019-05-07 16:18:49 -0300

    _not working the verifWin

--- machine.js
+++ machine.js
@@ -11,35 +11,36 @@ document.getElementById("nove").addEventListener("click", gameTicTac)
 const player = ["x", "o"]
 const pl1 = [] // pl1 = O
 const pl2 = [] // pl2 = X
+const possiblewins = [
+    ["um", "dois", "tres"],
+    ["um", "quatro", "sete"],
+    ["um", "cinco", "nove"],
+    ["dois", "cinco", "oito"],
+    ["quatro", "cinco", "seis"],
+    ["sete", "oito", "nove"],
+    ["sete", "cinco", "tres"],
+]
 
-// function verifWin() {
-//     const possiblewins = [
-//         ["um", "dois", "tres"],
-//         ["um", "quatro", "sete"],
-//         ["um", "cinco", "nove"],
-//         ["dois", "cinco", "oito"],
-//         ["quatro", "cinco", "seis"],
-//         ["sete", "oito", "nove"],
-//         ["sete", "cinco", "tres"],
-//     ]
-
-//     possiblewins.forEach(element => {
-//         if (pl1.find(element) || pl2.find(element)) {
-//             alert("Vencedor!")
-//         } else {
-//             alert("IH, DEU VELHA")
-//         }
-//     });
-// }
+function verifWin() {
+    possiblewins.forEach(element => {
+        if (pl1.equals(element) || pl2.equals(element)) {
+            alert("Jogo encerrado! Temos um vencedor.")
+        }
+        if (pl1[5] || pl2[5]) {
+            alert("Ops! Encontramos um erro")
+        }
+    })
+}
 
 function gameTicTac() {
-    // verifWin()
     const playAlternate = player.reverse()
     this.className += " " + playAlternate[0]
     if (playAlternate[0] === "x") {
         pl2.push(this.id)
+        verifWin()
     } else {
         pl1.push(this.id)
+        verifWin()
     }
 
     console.log(pl1)
