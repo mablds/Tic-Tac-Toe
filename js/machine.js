d40379fa722ec2416dc6f63eafa36a7d1e2e8933
Marcelo Arthur <marcelobragalemos@gmail.com>
2019-05-08 14:29:13 -0300

    SOLVED MAhMEN

--- js/machine.js
+++ js/machine.js
@@ -12,33 +12,57 @@ const player = ["x", "o"]
 const pl1 = [] // pl1 = O
 const pl2 = [] // pl2 = X
 const finalArray = []
+let jogadas = 0;
 
-function verifWin(winArr, playerArr) {
-    let verif = true
-    console.log('iniciandoverificação')
+function verifWin(winArr, playerArr, jogada) {
+    let verif = false
     winArr.map(e => {
         if (!playerArr.includes(e)) {
-            console.log('iguais')
-            verif = false
+            // console.log('iguais')
             if (playerArr.includes(e[0]) && playerArr.includes(e[1]) && playerArr.includes(e[2])) {
-                alert("Temos um vencedor!")
+                verif = true
             }
         }
     })
+
     if (verif == true) {
-        console.log('diferentes')
+        alert('Temos um Vencedor!')
+        console.log('nhe')
+        location.reload()
+    }
+    if (jogada > 8 && verif == false) {
+        console.log('DEU VELHA!')
+        alert('DEU VELHA!')
+        location.reload()
     }
 }
 
 function gameTicTac() {
-    const playAlternate = player.reverse()
-    this.className += " " + playAlternate[0]
-    console.log(this.className)
-    if (playAlternate[0] === "x") {
-        pl2.push(this.id)
-        verifWin(possiblewins, pl2)
-    } else {
-        pl1.push(this.id)
-        verifWin(possiblewins, pl1)
+    const playAlternate = player
+    if (playAlternate[0] === "x") { //playerAlternate[0] = ["x", "o"]
+        if (this.className.includes(" x") || this.className.includes(" o")) {
+            // playAlternate.reverse()
+            console.log("opção inválida! escolha um campo vazio. próximo a jogar: "+playAlternate[1])
+        } else {
+            this.className += " " + playAlternate[0]
+            pl2.push(this.id)
+            jogadas = jogadas + 1;
+            playAlternate.reverse()
+            console.log("proximo a jogar: "+playAlternate[0])
+            verifWin(possiblewins, pl2, jogadas)
+        }
     }
+    if (playAlternate[0] === "o") //playerAlternate[0] = ["o", "x"]
+        if (this.className.includes(" x") || this.className.includes(" o")) {
+            // playAlternate.reverse()
+            console.log("opção inválida! escolha um campo vazio. próximo a jogar: "+playAlternate[1])
+        } else {
+            this.className += " " + playAlternate[0]
+            pl1.push(this.id)
+            jogadas = jogadas + 1;
+            playAlternate.reverse()
+            console.log("proximo a jogar: "+playAlternate[0])
+            verifWin(possiblewins, pl1, jogadas)
+        }
 }
+
