1e28efc9f0142397d01077f6c05c88f4287af534
MARCELO ARTHUR BRAGA LEMOS DOS SANTOS <marcelo.ablsantos@senacsp.edu.br>
2019-05-09 21:10:31 -0300

    Second round pl1 and pl2 arrays bug

--- js/machine.js
+++ js/machine.js
@@ -1,6 +1,6 @@
-const player = ["x", "o"]
-let pl1 = [] // pl1 = O
-let pl2 = [] // pl2 = X
+let player = ["x", "o"]
+var pl1 = [] // pl1 = O
+var pl2 = [] // pl2 = X
 const finalArray = []
 let jogadas = 0;
 let playerO = 0
@@ -31,37 +31,51 @@ function start() {
 
 function gameTicTac() {
     const playAlternate = player
+    if(playerO > 0 || playerX > 0){
+        playAlternate.reverse()
+    }
+    console.log(playAlternate[0])
     if (playAlternate[0] === "x") { //playerAlternate[0] = ["x", "o"]
         if (this.className.includes(" x") || this.className.includes(" o")) {
             console.log("opção inválida! escolha um campo vazio. próximo a jogar: " + playAlternate[1])
         } else {
+            if(playerO > 0 || playerX > 0){
+                playAlternate.reverse()
+            }
             this.className += " " + playAlternate[0]
             pl2.push(this.id)
             jogadas = jogadas + 1;
             // playAlternate.reverse()
             console.log("proximo a jogar: " + playAlternate[0])
-            verifWin(possiblewins, pl2, jogadas)
-            playAlternate.reverse()
+            let verify = verifWin(possiblewins, pl2, jogadas)
+            console.log("pl2: "+pl2)
+            if (verify == false) {
+                playAlternate.reverse()
+            }
         }
     }
     if (playAlternate[0] === "o") //playerAlternate[0] = ["o", "x"]
         if (this.className.includes(" x") || this.className.includes(" o")) {
             console.log("opção inválida! escolha um campo vazio. próximo a jogar: " + playAlternate[1])
         } else {
+            if(playerO > 0 || playerX > 0){
+                playAlternate.reverse()
+            }
             this.className += " " + playAlternate[0]
             finalArray.push(playAlternate[0])
             pl1.push(this.id)
             jogadas = jogadas + 1;
             console.log("proximo a jogar: " + playAlternate[0])
-            verifWin(possiblewins, pl1, jogadas)
-            playAlternate.reverse()
+            let verify = (verifWin(possiblewins, pl1, jogadas))
+            console.log("pl1: "+pl1)
+            if (verify == false) {
+                playAlternate.reverse()
+            }
         }
-    
-
 }
 
 function verifWin(winArr, playerArr, jogada) {
-
+    let verifando = false
     const ultimoKey = finalArray.length
     const finalpositionArray = finalArray[ultimoKey - 1]
 
@@ -75,9 +89,10 @@ function verifWin(winArr, playerArr, jogada) {
     })
 
     if (verif == true) {
+        verifando = true
         const vectorAux = []
         const zeraJogadas = 0
-        setTimeout(function () { alert("PARABÉNS! Temos um vencedor!") }, 200)
+        setTimeout(function () { alert("PARABÉNS! Temos um vencedor!") }, 500)
         if (finalpositionArray == "o") {
             playerO += 1
             document.getElementById("playerO").innerHTML += "<br>O wins: " + playerO
@@ -86,20 +101,24 @@ function verifWin(winArr, playerArr, jogada) {
             playerX += 1
             document.getElementById("playerX").innerHTML += "<br>X wins: " + playerX
         }
-        document.getElementById("um").className = 'square'
-        document.getElementById("dois").className = 'square'
-        document.getElementById("tres").className = 'square'
-        document.getElementById("quatro").className = 'square'
-        document.getElementById("cinco").className = 'square'
-        document.getElementById("seis").className = 'square'
-        document.getElementById("sete").className = 'square'
-        document.getElementById("oito").className = 'square'
-        document.getElementById("nove").className = 'square'
-        pl1 = vectorAux
-        pl2 = vectorAux
-        jogadas = zeraJogadas
+        setTimeout(() => {
+            document.getElementById("um").className = 'square'
+            document.getElementById("dois").className = 'square'
+            document.getElementById("tres").className = 'square'
+            document.getElementById("quatro").className = 'square'
+            document.getElementById("cinco").className = 'square'
+            document.getElementById("seis").className = 'square'
+            document.getElementById("sete").className = 'square'
+            document.getElementById("oito").className = 'square'
+            document.getElementById("nove").className = 'square'
+            pl1 = vectorAux
+            pl2 = vectorAux
+            jogadas = zeraJogadas
+        }, 1000)
+
     }
     if (jogada > 8 && verif == false) {
-        setTimeout(function () { alert('DEU VELHA!') }, 300)
+        setTimeout(function () { alert('DEU VELHA!') }, 800)
     }
+    return verifando
 }
